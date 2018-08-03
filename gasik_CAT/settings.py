e5131f4500dd47c52adf3f105c95cda4f596b9c3
Fauzanil Zaki <fauzanil@live.com>
2018-08-03 07:34:08 +0700

    bug fixes

--- gasik_CAT/settings.py
+++ gasik_CAT/settings.py
@@ -131,6 +131,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 LOGIN_URL = '/auth/login'
 LOGOUT_REDIRECT_URL  = '/auth/login'
